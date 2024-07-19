45d8122b01aeac0b29d853dbcb515f209524c341
Nisarg2061 <khodkenisarg@gmail.com>
2024-07-19 08:56:13 +0530

    added new tests to test files

--- check.go
+++ check.go
@@ -12,7 +12,7 @@ func Check(destination string, port string) string {
   _, err := net.DialTimeout("tcp", address, timeout)
   var status string
  if err != nil {
-    status = fmt.Sprintf("[DOWN] %v is unreachable, \n Error: %v \n", destination, err)
+    status = fmt.Sprintf("[DOWN] %v is unreachable.\n", destination)
   }  else {
     status = fmt.Sprintf("[UP] %v is reachable.\n", destination)
   }
--- check_test.go
+++ check_test.go
@@ -8,9 +8,10 @@ import (
 func TestCheck(t *testing.T){
   destination := "google.com"
   expected := fmt.Sprintf("[UP] %v is reachable.\n", destination)
+  expected2 := fmt.Sprintf("[DOWN] %v is unreachable.\n", destination)
   result := Check("google.com", "80")
 
-  if result != expected {
-    t.Error("Expected: " + expected + " got: " + result)
+  if result != expected && result != expected2{
+    t.Error(fmt.Sprintf("Expected %v or %v got %v", expected, expected2, result))
   }
 }
