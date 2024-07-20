f9009b0fd24063b7d259ae67e3f86bda0fbccedb
Nisarg2061 <khodkenisarg@gmail.com>
2024-07-20 11:57:01 +0530

    updated test files

--- check_test.go
+++ check_test.go
@@ -5,24 +5,12 @@ import (
 	"testing"
 )
 
-func TestCheckReachable(t *testing.T){
+func TestCheck(t *testing.T){
   destination := "synfullsolutions.com"
   expected := fmt.Sprintf("[UP] %v is reachable.\n", destination)
   expected2 := fmt.Sprintf("[DOWN] %v is unreachable.\n", destination)
   //uses an unreachable host
   result := Check("synfullsolutions.com", "80")
-
-  if result != expected && result != expected2{
-    t.Error(fmt.Sprintf("Expected %v or %v got %v", expected, expected2, result))
-  }
-}
-
-func TestCheckUnreachable(t *testing.T){
-  destination := "google.com"
-  expected := fmt.Sprintf("[UP] %v is reachable.\n", destination)
-  expected2 := fmt.Sprintf("[DOWN] %v is unreachable.\n", destination)
-  //uses an unreachable host
-  result := Check("google.com", "80")
 
   if result != expected && result != expected2{
     t.Error(fmt.Sprintf("Expected %v or %v got %v", expected, expected2, result))
