88267cc7c88789fb5ea170866181122fa1063406
Nisarg2061 <khodkenisarg@gmail.com>
2024-07-22 19:10:13 +0530

    Add Comments

--- check.go
+++ check.go
@@ -7,14 +7,14 @@ import (
 )
 
 func Check(destination string, port string) string {
-  address := destination + ":" + port
-  timeout := time.Duration(5 * time.Second)
-  _, err := net.DialTimeout("tcp", address, timeout)
+  address := destination + ":" + port // taking the input from values passed by the user
+  timeout := time.Duration(5 * time.Second) // duration after which it will be declared unreachable
+  _, err := net.DialTimeout("tcp", address, timeout) // the output determines if unreachable or reachable
   var status string
   if err != nil {
-    status = fmt.Sprintf("[DOWN] %v is unreachable.\n", destination)
+    status = fmt.Sprintf("[DOWN] %v is unreachable.\n", destination) // Status if the domain is not reachable
   }  else {
-    status = fmt.Sprintf("[UP] %v is reachable.\n", destination)
+    status = fmt.Sprintf("[UP] %v is reachable.\n", destination) // Status if the doamin is reachable
   }
-  return status
+  return status // returning the status of the website
 }
