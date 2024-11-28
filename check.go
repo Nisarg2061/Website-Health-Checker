ef8b99140f86d736cb2e38d4ab6a112ba1300284
Nisarg <khodkenisarg@gmail.com>
2024-11-29 00:12:59 +0530

    add frmt

--- check.go
+++ check.go
@@ -7,14 +7,23 @@ import (
 )
 
 func Check(destination string, port string) string {
-  address := destination + ":" + port // taking the input from values passed by the user
-  timeout := time.Duration(5 * time.Second) // duration after which it will be declared unreachable
-  _, err := net.DialTimeout("tcp", address, timeout) // the output determines if unreachable or reachable
-  var status string
-  if err != nil {
-    status = fmt.Sprintf("[DOWN] %v is unreachable.\n", destination) // Status if the domain is not reachable
-  }  else {
-    status = fmt.Sprintf("[UP] %v is reachable.\n", destination) // Status if the doamin is reachable
-  }
-  return status // returning the status of the website
+	address := destination + ":" + port // taking the input from values passed by the user
+	timeout := time.Duration(
+		5 * time.Second,
+	) // duration after which it will be declared unreachable
+	_, err := net.DialTimeout(
+		"tcp",
+		address,
+		timeout,
+	) // the output determines if unreachable or reachable
+	var status string
+	if err != nil {
+		status = fmt.Sprintf(
+			"[DOWN] %v is unreachable.\n",
+			destination,
+		) // Status if the domain is not reachable
+	} else {
+		status = fmt.Sprintf("[UP] %v is reachable.\n", destination) // Status if the doamin is reachable
+	}
+	return status // returning the status of the website
 }
--- check_test.go
+++ check_test.go
@@ -5,24 +5,24 @@ import (
 	"testing"
 )
 
-func TestCheckReachable(t *testing.T){
-  destination := "google.com"
-  expected := fmt.Sprintf("[UP] %v is reachable.\n", destination)
-  //uses an reachable host
-  result := Check("google.com", "80")
+func TestCheckReachable(t *testing.T) {
+	destination := "google.com"
+	expected := fmt.Sprintf("[UP] %v is reachable.\n", destination)
+	// uses an reachable host
+	result := Check("google.com", "80")
 
-  if result != expected {
-    t.Error(fmt.Sprintf("Expected %v got %v", expected, result))
-  }
+	if result != expected {
+		t.Error(fmt.Sprintf("Expected %v got %v", expected, result))
+	}
 }
 
-func TestCheckUnreachable(t *testing.T){
-  destination := "synfullsolutions.com"
-  expected2 := fmt.Sprintf("[DOWN] %v is unreachable.\n", destination)
-  //uses an unreachable host
-  result := Check("synfullsolutions.com", "80")
+func TestCheckUnreachable(t *testing.T) {
+	destination := "synfullsolutions.com"
+	expected2 := fmt.Sprintf("[DOWN] %v is unreachable.\n", destination)
+	// uses an unreachable host
+	result := Check("synfullsolutions.com", "80")
 
-  if result !=  expected2{
-    t.Error(fmt.Sprintf("Expected %v got %v", expected2, result))
-  }
+	if result != expected2 {
+		t.Error(fmt.Sprintf("Expected %v got %v", expected2, result))
+	}
 }
